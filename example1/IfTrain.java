5232973372ec319c8567ef9b62b5c0a6f9206940
Suleyman Demir <sulodemir.47@gmail.com>
2022-08-27 00:35:17 +0300

    toplamaislemi

--- example1/IfTrain.java
+++ example1/IfTrain.java
@@ -1,14 +1,41 @@
 package example1;
-public class IfTrain {
-    
-    public static void main(String[] args){
-        
-        int sayi = 5;
 
-        if(sayi >4){
-            System.out.println("büyük 4");
-        } else {
-            System.out.println("küçük esit 4");
-        }
+import java.util.Scanner;
+
+public class IfTrain {
+
+    int basla = 1;
+
+    byte byteSayi = 1;
+    short kisaSayi = 123;
+    int sayi = 1999999999;
+    long buyukSayi = 1999999999;
+    float noktaliSayi = 1234234535323423423.5f;
+    double virgulluSayi = 12323442342423423423423423423423423423423424324242342343232432432423423432432423432423425467643.9d;
+    char harf = 'x';
+    static String yazi = "Yazi selam. 123 naber.";
+    boolean dogru = true;
+    boolean yanlis = false;
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("ilk sayiyi girin:");
+        int ilkSayi = scanner.nextInt();
+
+        System.out.println("ikinci sayiyi girin:");
+        int ikinciSayi = scanner.nextInt();
+
+        int sonuc = toplamaIslemi(ilkSayi, ikinciSayi);
+
+        System.out.println("sonuc:" + sonuc);
+
+    }
+
+    int sonuc = 0;
+
+    public static int toplamaIslemi(int birincisayi, int ikinciSayi) {
+        int sonuc = birincisayi + ikinciSayi;
+        return sonuc;
     }
 }
