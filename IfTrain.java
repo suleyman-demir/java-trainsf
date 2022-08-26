fa9aab5942b540236bdca1bcc79f76e9d1d4dfbe
Suleyman Demir <sulodemir.47@gmail.com>
2022-08-26 23:35:36 +0300

    if örneği

---
+++ IfTrain.java
@@ -0,0 +1,13 @@
+public class IfTrain {
+    
+    public static void main(String[] args){
+        
+        int sayi = 5;
+
+        if(sayi >4){
+            System.out.println("büyük 4");
+        } else {
+            System.out.println("küçük esit 4");
+        }
+    }
+}
