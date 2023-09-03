732d6f165e7db74a72362571a89d891d1af0bd6c
Suleyman Demir <sulodemir.47@gmail.com>
2023-09-03 21:05:30 +0300

    tasklar

--- tasklar/VeriTipleri.java
+++ tasklar/VeriTipleri.java
@@ -10,8 +10,18 @@ public class VeriTipleri {
         float mfloat=45.6f;
         boolean mboolean = false;
         String sstring="VERİ TİPLERİ VE DEĞİŞKENLER";
+        Integer iinteger=null;
+        Double Ddouble=new Double(1.47);
+        Float Ffloat=2.47f;
+        Long Llong=1234567890123L;
+        Byte Bbyte=120;
+        Boolean Bboolean= null;
 
-        System.out.println(dint+"\n"+cchar+"\n"+mbyte+"\n"+mlong+"\n"+mfloat+"\n"+sstring+"\n"+bdouble );
+
+
+
+        System.out.println(dint+"\n"+cchar+"\n"+mbyte+"\n"+mlong+"\n"+mfloat+"\n"+sstring+"\n"+bdouble+"\n"+iinteger+"\n"+Ddouble+"\n"+Ffloat+"\n"+Llong+"\n"+
+                Bbyte+"\n"+Bboolean);
 
     }
 }
