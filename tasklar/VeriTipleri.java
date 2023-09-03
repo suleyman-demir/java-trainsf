aa05856f622143e60f1c8df0db727c477705c431
Suleyman Demir <sulodemir.47@gmail.com>
2023-09-03 20:33:52 +0300

    tasklar

---
+++ tasklar/VeriTipleri.java
@@ -0,0 +1,17 @@
+package tasklar;
+
+public class VeriTipleri {
+    public static void main(String[] args) {
+        int dint=100;
+        double bdouble=1.06;
+        char cchar='S';
+        byte mbyte=126;
+        long mlong=10000;
+        float mfloat=45.6f;
+        boolean mboolean = false;
+        String sstring="VERİ TİPLERİ VE DEĞİŞKENLER";
+
+        System.out.println(dint+"\n"+cchar+"\n"+mbyte+"\n"+mlong+"\n"+mfloat+"\n"+sstring+"\n"+bdouble );
+
+    }
+}
